16bcd059b2fe35f82059074aa510b4a851e56c79
3lswear <r.malyavsky@protonmail.com>
2020-12-24 06:52:33 +0300

    moved code to CryptoUtil, finalized 2-way encryption

--- src/com/roma/Client.java
+++ src/com/roma/Client.java
@@ -8,58 +8,15 @@ import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
-import static com.roma.CryptoUtil.printHex;
-//import static com.roma.Server.encrypt;
+import static com.roma.CryptoUtil.*;
 
 
 public class Client {
 
-    private static final String ALGORITHM = "RSA";
-
-    public static byte[] encrypt(byte[] publicKey, byte[] inputData) throws Exception {
-
-        PublicKey key = KeyFactory.getInstance(ALGORITHM)
-                .generatePublic(new X509EncodedKeySpec(publicKey));
-
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, key);
-
-        byte[] encryptedBytes = cipher.doFinal(inputData);
-
-        return encryptedBytes;
-    }
-
-    public static byte[] decrypt(byte[] privateKey, byte[] inputData)
-            throws Exception {
-
-        PrivateKey key = KeyFactory.getInstance(ALGORITHM)
-                .generatePrivate(new PKCS8EncodedKeySpec(privateKey));
-
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, key);
-
-        byte[] decryptedBytes = cipher.doFinal(inputData);
-
-        return decryptedBytes;
-    }
-
-    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException {
-
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
-
-        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
-
-        // 512 is keysize
-        keyGen.initialize(512, random);
-
-        KeyPair generateKeyPair = keyGen.generateKeyPair();
-        return generateKeyPair;
-    }
-
     public static void main(String args[]) throws Exception {
 
-        Socket s = new Socket("localhost", 31337);
-        System.out.println("[+] Connected to 31337");
+        Socket s = new Socket("localhost", port);
+        System.out.println("[+] Connected to " + port);
 
         DataInputStream din = new DataInputStream(s.getInputStream());
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
@@ -74,20 +31,15 @@ public class Client {
 
         System.out.println("[+] Sending pubkey...");
 //        System.out.println("my pubkey: " + new String(publicKey));
-        dout.writeInt(publicKey.length);
-        dout.write(publicKey);
+        sendBytes(publicKey, dout);
 
         System.out.println("[+] Recieving pubkey...");
-        int fKeyLength = din.readInt();
-        if (fKeyLength > 0) {
-            foreignKey = new byte[fKeyLength] ;
-            din.readFully(foreignKey, 0, fKeyLength); // read the message
-        }
+        foreignKey = receiveBytes(din);
 //        System.out.println("recieved pubkey ->" + new String(foreignKey));
 
         String sendstr = "";
         String recstr = "";
-        while (!sendstr.equals("stop")) {
+        while (!recstr.equals("stop") && !sendstr.equals("stop")) {
             System.out.print("[client] Send a message -> ");
             sendstr = br.readLine();
 
@@ -95,13 +47,13 @@ public class Client {
             System.out.println("[+] Encrypting message: ");
             printHex(encryptedMsg);
 
-            dout.writeInt(encryptedMsg.length);
-            dout.write(encryptedMsg);
-//            dout.writeUTF(sendstr);
-            dout.flush();
+            sendBytes(encryptedMsg, dout);
 
-            recstr = din.readUTF();
-            System.out.println("[+] Server\'s response: " + recstr);
+            encryptedMsg = receiveBytes(din);
+            System.out.print("[+] Recieved encrypted message: ");
+            printHex(encryptedMsg);
+            recstr = new String(decrypt(privateKey, encryptedMsg));
+            System.out.println("[+] Client\'s response: " + recstr);
         }
 
         System.out.println("[+] Connection aborted by user");
